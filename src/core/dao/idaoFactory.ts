41f3f26b1ea16d598234991ade9e260145e6b727
josselin chevalay <josselin54.chevalay@gmail.com>
2017-04-04 19:17:02 +0200

    from scratch

--- src/core/dao/idaoFactory.ts
+++
@@ -1,8 +0,0 @@
-export enum PERSISTENCE_MODE {
-    EVENT_SYNC,
-    EVENT_ASYNC
-}
-
-export interface IDaoFactory<T> {
-    getDao(mode : PERSISTENCE_MODE) : T
-}
--- typings/tsd.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="electron/electron.d.ts" />
