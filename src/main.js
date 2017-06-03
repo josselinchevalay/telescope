8ab4e9a10b199ddf620eeaef85f80f848840ed0c
josselin chevalay <josselin54.chevalay@gmail.com>
2017-06-03 17:25:17 +0200

    display ipfs PeerID

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
 import Telescope from './telescope';
-import IpfsTopics from './services/api/event/EventIpfs/topics';
 
-const {ipcRenderer} = require('electron');
-
-/*ipcRenderer.on(IpfsTopics.CONFIG_GET_RESPONSE, (event, arg) => {
-  console.log(JSON.parse(arg)) 
-});*/
-
-//ipcRenderer.send(IpfsTopics.CONFIG_GET, '');
-console.log(JSON.parse(ipcRenderer.sendSync(IpfsTopics.CONFIG_GET, '')));
 let telescope = new Telescope();
 telescope.initialize();
 
