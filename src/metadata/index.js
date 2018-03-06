ac989c88e32ca0fecf39df35179dc82feec29f14
josselinchevalay <josselin54.chevalay@gmail.com>
2018-03-06 20:47:24 +0100

    upgrade moment to fix vulnerability

--- src/metadata/index.js
+++ src/metadata/index.js
@@ -15,6 +15,21 @@ export default class AppBody extends Component{
 		};
 		this.getMetadata();
 		ipcRenderer.on(NotificationTopics.REFRESH_METADA, this.getMetadata);
+		this.editHandler = this.edit.bind(this);
+		this.changeHandler = this.change.bind(this);
+	}
+
+	change(event){
+		console.log(event.target);
+	}
+
+	getHTMLInputTemplate(namedNodeHTML){
+		return "<input type='text' value='"+namedNodeHTML.dataset.value+"' onChange={this.changeHandler} />";
+	}
+
+	edit(event){
+		let namedNodeHTML = event.target;
+		event.target.innerHTML = this.getHTMLInputTemplate(namedNodeHTML);
 	}
 
 	getMetadata(){
@@ -32,14 +47,26 @@ export default class AppBody extends Component{
     			      <tr>
     			         <th>Name</th>
     			         <th>Value</th>
+    			         <th>Action</th>
     			      </tr>
     			   </thead>
     			   <tbody>
 	    			   {
 	    			   	   Object.keys(this.state.metadata).map((metaName) => {
-	    			   	   	   return <tr><th scope="row">{metaName}</th><td>{this.state.metadata[metaName]}</td></tr>;
+	    			   	   	   return <tr id={metaName}><th scope="row">{metaName}</th><td data-row={metaName} data-value={this.state.metadata[metaName]} className="value" onClick={this.editHandler} >{this.state.metadata[metaName]}</td><td><a href="#" className="btn btn-danger">Delete</a></td></tr>;
 	    			   	   })
 	    			   }
+	    			   <tr className="form-group">
+	    			      <td>
+	    			          <input type="text" className="form-control-plaintext"  id="nameMeta" />
+	    			      </td>
+	    			      <td>
+	    			          <input type="text" className="form-control-plaintext" id="metaValue" />
+	    			      </td>
+	    			      <td>
+	    			           <a href="#" className="btn btn-success">Save</a>
+	    			      </td>
+	    			   </tr>
 	    			</tbody>
     			</table>
     		</div>
