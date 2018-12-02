6b42e7e7dd94386f1209103506af4cb247637435
josselinchevalay <josselin54.chevalay@gmail.com>
2018-12-02 17:03:35 +0100

    add file components refactored

--- src/add-file/add_file.js
+++ src/add-file/add_file.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import IpfsTopics from '../services/api/event/EventIpfs/topics';
+import {forEach, map} from 'lodash';
+
+const {ipcRenderer} = require('electron');
 
 export default class AddFileButton extends Component {
   constructor(props){
     super(props);
-    this.state = {};
     this.buttonAddFileClickHandler = this.buttonAddFileClick.bind(this);
+    this.buttonFileCHangeHandler = this.buttonFileCHange.bind(this);
   }
 
   componentDidMount() {
-    console.log('hello');
+    let state = {};
+    state.files = [];
+    this.setState(state);
   }
 
   buttonAddFileClick(event) {
     let element = document.getElementsByClassName('input-file')[0].click();
+  }
+
+  convertFile(f) {
+    return JSON.stringify({
+      name : f.name,
+      path : f.path,
+      lastModified : f.lastModified,
+      size : f.size,
+      type : f.type
+    });
+  }
+
+  buttonFileCHange(event) {
+    let files = event.target.files;
+    files = map(files, this.convertFile);
+    ipcRenderer.send(IpfsTopics.DOCUMENT_ADD, JSON.stringify(files));
   }
 
   render(){
@@ -21,7 +43,7 @@ export default class AddFileButton extends Component {
         <a href="#" className="btn btn-small btn-success" onClick={this.buttonAddFileClickHandler}>
              <i className="fa fa-plus"></i>&nbsp;
              <span>Add Files</span>
-             <input type="file" className="input-file" name="myFile"/>
+             <input type="file" className="input-file" name="myFile" onChange={this.buttonFileCHangeHandler}/>
          </a>
       </div>
     );
